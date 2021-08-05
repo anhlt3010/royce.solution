f3fe974fc8965a8d167c38917f18a2a96107cfda
itsmeny <chokjaroen.estk@mail.kmutt.ac.th>
2021-08-05 07:00:06 +0700

    royce.solution

---
+++ royce.solution/MainFrm.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace royce.solution
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadbypass_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadbypass_btn
+            // 
+            this.loadbypass_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.loadbypass_btn.Location = new System.Drawing.Point(12, 12);
+            this.loadbypass_btn.Name = "loadbypass_btn";
+            this.loadbypass_btn.Size = new System.Drawing.Size(274, 57);
+            this.loadbypass_btn.TabIndex = 0;
+            this.loadbypass_btn.Text = "Bypass";
+            this.loadbypass_btn.UseVisualStyleBackColor = true;
+            this.loadbypass_btn.Click += new System.EventHandler(this.loadbypass_btn_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 89);
+            this.Controls.Add(this.loadbypass_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "[royce.solution]";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadbypass_btn;
+    }
+}
+
---
+++ royce.solution/MainFrm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace royce.solution // by royce#1607
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        #region "DLLImport"
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, int lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll")]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetModuleHandleA(string lpModuleName);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+        #endregion
+
+        private void loadbypass_btn_Click(object sender, EventArgs e)
+        {
+            IntPtr Ragexe_ProcessHandle = Process.GetProcessesByName("Ragexe")[0].Handle;
+            IntPtr Ragexe_BaseAddress = Process.GetProcessesByName("Ragexe")[0].MainModule.BaseAddress;
+            int[] pointer = { 0x921C94, 0x921CA0, 0x921CAC, 0x921CC4, 0xBDC96D };
+            byte[] toWrite = { 0x72, 0x6F, 0x79, 0x63, 0x65 };
+            byte[] read = new byte[4];
+            IntPtr KERNEL32_READPROCESSMEMORY = GetProcAddress(GetModuleHandleA("kernel32.dll"), "ReadProcessMemory");
+
+            for (int i = 0; i < pointer.Length; i++)
+            {
+                WriteProcessMemory(Ragexe_ProcessHandle, Ragexe_BaseAddress + pointer[i], toWrite, toWrite.Length, 0);
+            }
+
+            WriteProcessMemory(Ragexe_ProcessHandle, KERNEL32_READPROCESSMEMORY, new byte[] { 0xEB, 0xFE }, 2, 0);
+            ReadProcessMemory(Ragexe_ProcessHandle, KERNEL32_READPROCESSMEMORY, read, read.Length, 0);
+
+            if ((BitConverter.ToInt32(read, 0)) == -1957298453) // Check bypass status from KERNEL32_READPROCESSMEMORY
+            {
+                loadbypass_btn.Text = "Bypass Successfully!";
+            }
+            else
+            {
+                loadbypass_btn.Text = "Bypass Failed!";
+            }
+
+            // INJECT DLL (Manual inject dll or code it by yourself or whatever)
+        }
+    }
+}
